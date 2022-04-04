d1dbd18e9d174dd3682d9b6fe83cdb9aea93afc1
unknown <jimikill3d@gmail.com>
2022-04-04 16:34:13 +0700

    add more stuff:

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-tes
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 80;
+
+
+app.get('/', (req, res) => {
+    res.sendFile('out.html', { root: __dirname });
+
+
+
+
+
+});
+app.get('/:keyword', (req, res) => {
+  
+})
+
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
+});
+
+
